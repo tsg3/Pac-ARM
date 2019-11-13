01c98a3017722a4a624c84b92de179bf260ceb88
Andrey <diego.garro120@gmail.com>
2019-11-13 16:35:09 -0600

    Se completa la ALU con las intrucciones hasta el momento.

--- complemento.sv
+++ complemento.sv
@@ -1,10 +1,6 @@
 module complemento(input logic [31:0] dato,
 						 output logic [31:0] dato_complemento);
 	
-		
-		always_comb begin
-			if(dato == 32'd0) dato_complemento = dato;
-			else dato_complemento = ~dato;				
-		end
+		assign dato_complemento = ~dato;
 	
 endmodule
