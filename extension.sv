ca02000fb4431765bdc45ac1bb41acb3c08286d2
Esteban <estebandcg1999@gmail.com>
2019-11-19 17:25:44 -0600

    Arreglo del módulo de extensión de cero y signo

--- extension.sv
+++ extension.sv
@@ -1,12 +1,14 @@
 module extension #(parameter BITS = 16) (input logic [BITS - 1:0] datoIn,
 													  input logic logicalOperation,
 													  output logic [31:0] datoExt);
-			
+	
+	int i;
+	
 	always_comb begin
-		if(~logicalOperation && datoIn[BITS - 1] == 1)
-			datoExt = 32'd1 & datoIn;
+		if(~logicalOperation)
+			datoExt = 32'(signed'(datoIn));
 		else
-			datoExt = 32'd0 | datoIn;
+			datoExt = datoIn;
 	end
 	
 endmodule
